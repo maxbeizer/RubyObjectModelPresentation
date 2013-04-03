b5761ae15a8806250a550094ac78f85343609544
Max Beizer <max.beizer@gmail.com>
2013-04-02 20:40:54 -0500

    add other classes

--- example.rb
+++ example.rb
@@ -1,7 +1,29 @@
 class NssStudent
-  attr_accessor :name
+  attr_reader :name, :job, :experience
 
-  def initialize(name)
+  def initialize(name, job, experience)
     @name = name
+    @job = job
+    @experience = experience
+  end
+end
+
+class JrDev
+  attr_reader :name, :job, :experience
+
+  def initialize(name, job, experience)
+    @name = name
+    @job = job
+    @experience = experience
+  end
+end
+
+class AccomplishedProfessional
+  attr_reader :name, :job, :experience
+
+  def initialize(name, job, experience)
+    @name = name
+    @job = job
+    @experience = experience
   end
 end
