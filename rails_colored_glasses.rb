dd116427ecb10eed89defeda3bc6c9b18a6a3f2a
Max Beizer <max.beizer@gmail.com>
2013-06-22 18:20:13 -0500

    extract railsy into module

--- rails_colored_glasses.rb
+++ rails_colored_glasses.rb
@@ -1,35 +1,32 @@
 class Executive < ActiveRecord::Base
   #heaps of big important stuff omitted
-  def attend_meeting_at(meeting_place)
-    self.location = meeting_place
-  end
+  include Officeable
 
-  def answer_email
-    self.frustration_level += 1
-  end
-
-  def speak_in_front_of_large_groups(size)
-    self.confidence_level = size / 1
+  def amount_of_unease
+    1
   end
 end
 
 class MiddleManagement < ActiveRecord::Base
   #lots of mgmt stuff omitted
-  def attend_meeting_at(meeting_place)
-    self.location = meeting_place
-  end
+  include Officeable
 
-  def answer_email
-    self.frustration_level += 1
-  end
-
-  def speak_in_front_of_large_groups(size)
-    self.confidence_level = size / 2
+  def amount_of_unease
+    2
   end
 end
 
 class Developer < ActiveRecord::Base
   #general nerdery omitted
+  include Officeable
+
+  def amount_of_unease
+    100
+  end
+end
+
+#lib/officeable.rb
+module Officeable
   def attend_meeting_at(meeting_place)
     self.location = meeting_place
   end
@@ -39,6 +36,6 @@ class Developer < ActiveRecord::Base
   end
 
   def speak_in_front_of_large_groups(size)
-    self.confidence_level = size / 100
+    self.confidence_level = size / amount_of_unease
   end
 end
